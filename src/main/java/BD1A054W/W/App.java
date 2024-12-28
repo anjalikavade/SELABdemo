533a8f4f501d1248d5b6ecfb448f4ad9af752f56
Anjaliikavade <kavadeanjali19012005@gmail.com>
2024-12-28 21:43:13 +0530

    Initial commit of Maven project

---
+++ src/main/java/BD1A054W/W/App.java
@@ -0,0 +1,10 @@
+package BD1A054W.W;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Anjali!");
+    }
+}
---
+++ src/test/java/BD1A054W/W/AppTest.java
@@ -0,0 +1,19 @@
+package BD1A054W.W;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
